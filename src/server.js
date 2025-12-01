b9e7d2cf808ab481a7496090f415b4ab872d7ef0
Aayush Ranjan <110541448+AayushRanjan31@users.noreply.github.com>
2025-12-01 23:32:27 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const sendRoute = require("./routes/send");
 
 const app = express();
 
-const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "*";
+const FRONTEND_ORIGIN = "https://ultra-pro-mailer-frontend-production.up.railway.app/";
 app.use(cors({ origin: FRONTEND_ORIGIN }));
 
 app.use(express.json({ limit: "10mb" }));
