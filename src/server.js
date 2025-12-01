f613873070d79bfe0f0dbd3083d4d652632e7e6e
Aayush Ranjan <110541448+AayushRanjan31@users.noreply.github.com>
2025-12-02 00:16:23 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,5 +16,9 @@ app.use(cors({
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("Ultra Pro Mailer backend is running!");
+});
+
 app.use("/send", sendRoute);
 
